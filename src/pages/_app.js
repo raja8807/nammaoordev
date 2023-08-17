5e5df4ea31d42ddf34b9ade4c74240444f28bd6e
Raja <s.rajarathinam1999@gmail.com>
2023-08-17 22:39:59 +0530

    font update

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,11 +1,17 @@
-import NodHeader from "@/components/layout/header";
 import "@/styles/globals.css";
 
 import { useEffect } from "react";
 import Aos from "aos";
-import 'aos/dist/aos.css'
+import "aos/dist/aos.css";
+
+import { Josefin_Sans } from "next/font/google";
+import Head from "next/head";
 // import { SessionProvider } from "next-auth/react";
 // import { SSRProvider } from "react-bootstrap";
+
+const josefin_Sans = Josefin_Sans({
+  subsets: ["latin"],
+});
 
 export default function App({ Component, pageProps }) {
   useEffect(() => {
@@ -18,10 +24,24 @@ export default function App({ Component, pageProps }) {
 
   return (
     <>
+      {/* <Head>
+        <style jsx global>{`
+          html {
+            p {
+
+              background-color: #161925;
+              font-family: ${josefin_Sans.style.fontFamily};
+            }
+          }
+        `}</style>
+      </Head> */}
+
       {/* <SessionProvider session={pageProps.session}> */}
       {/* <SSRProvider> */}
       {/* <NodHeader /> */}
-      <Component {...pageProps} />
+      <main className={josefin_Sans.className}>
+        <Component {...pageProps} />
+      </main>
       {/* </SSRProvider> */}
       {/* </SessionProvider> */}
     </>
